12c6fbf3929e502448ffea4183b9f93e1845334b
Austin <austinlapointe95@gmail.com>
2017-04-20 16:16:39 -0400

    match now works finally

--- lunchdate/matchingApp/urls.py
+++ lunchdate/matchingApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<person_id>[0-9]+)/$', views.queryOut, name='Query Output'),
     url(r'^(?P<person_id>[0-9]+)match/$', views.matchUser, name='MatchUser'),
+    url(r'^(?P<person_id>[0-9]+)/matchnow/$', views.matchUserNow, name='MatchUserNow'),
 ]
